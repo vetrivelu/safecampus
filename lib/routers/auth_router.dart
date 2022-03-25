ad8bec7c7c86c49e19ad62b6d2c57573ed35a042
ilaiya-raja <pitchuclassy@gmail.com>
2022-03-25 12:17:53 +0530

    user account deleted from admin grey screen removed

--- lib/routers/auth_router.dart
+++ lib/routers/auth_router.dart
@@ -11,28 +11,38 @@ import 'package:safecampus/screens/auth/verify_email.dart';
 import 'package:safecampus/screens/home.dart';
 import 'package:safecampus/screens/profile/profile_form.dart';
 
-class AuthRouter extends StatelessWidget {
+class AuthRouter extends StatefulWidget {
   const AuthRouter({Key? key}) : super(key: key);
 
+  @override
+  State<AuthRouter> createState() => _AuthRouterState();
+}
+
+class _AuthRouterState extends State<AuthRouter> {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<User?>(
       stream: auth.authStateChanges(),
       builder: (BuildContext context, AsyncSnapshot<User?> snapshot) {
-        if (snapshot.connectionState == ConnectionState.active && snapshot.hasData) {
+        if (snapshot.connectionState == ConnectionState.active &&
+            snapshot.hasData) {
           User user = snapshot.data!;
 
           if (true) {
             return StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
                 stream: users.doc(auth.uid).snapshots(),
                 builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.active && snapshot.hasData) {
-                    if (snapshot.data!.exists) {
-                      userController.user = UserModel.fromJson(snapshot.data!.data()!);
-                      if (userController.user != null) {
-                        return const Home();
-                      } else {
-                        return const ProfileForm();
+                  if (snapshot.connectionState == ConnectionState.active &&
+                      snapshot.hasData) {
+                    if (snapshot.data != null) {
+                      if (snapshot.data!.exists) {
+                        userController.user =
+                            UserModel.fromJson(snapshot.data!.data()!);
+                        if (userController.user != null) {
+                          return Home();
+                        } else {
+                          return const ProfileForm();
+                        }
                       }
                     }
                     return const ProfileForm();
