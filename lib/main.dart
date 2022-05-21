23385f6fc4ac27ca9cccd5d147827ae437dfbf5f
Vetrivelu Murugesan <75613547+vetrivelu@users.noreply.github.com>
2022-05-21 16:14:29 +0530

    emulator strike

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@
 
 import 'dart:typed_data';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
